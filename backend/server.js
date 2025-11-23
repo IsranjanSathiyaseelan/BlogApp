ffeeb8db82c234b3ff51442f53655d7fa4ea155a
IsranjanSathiyaseelan <isranjansathiyaseelan@gmail.com>
2025-11-23 21:39:07 +0530

    Mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
